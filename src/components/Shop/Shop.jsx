c653be6ec072b390a4edae2ee203b3ba85629326
mehedi9868 <mehedi9868@gmail.com>
2023-03-28 01:44:03 +0600

    store id and quantity to local storage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -20,6 +21,7 @@ const Shop = () => {
         // console.log('GOT PRODUCT:', product);
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
     }
 
     return (
