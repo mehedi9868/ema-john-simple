ae31b8f7e45e2c574207c200d312e0fe564ed696
mehedi9868 <mehedi9868@gmail.com>
2023-03-28 13:38:50 +0600

    update cart state with local storage data

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
+
     useEffect(() => {
         const loadData = async () => {
             const res = await fetch('products.json');
@@ -17,11 +18,26 @@ const Shop = () => {
         loadData();
     }, []);
 
+    // load from local storage:
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct['quantity'] = quantity;
+                savedCart.push(addedProduct);
+            }
+        }
+        setCart(savedCart);
+    }, [products]);
+
     const handleAddToCart = (product) => {
         // console.log('GOT PRODUCT:', product);
         const newCart = [...cart, product];
         setCart(newCart);
-        addToDb(product.id);
+        addToDb(product.id); // store to local storage
     }
 
     return (
